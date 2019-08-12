4d4088bbe193a38797a40ba6c7722318edd4f6bf
rotcivegaf <victorfage@gmail.com>
2019-08-12 12:15:58 -0300

    remove unused function

--- src/markets/Binance.js
+++ src/markets/Binance.js
@@ -8,10 +8,6 @@ module.exports = class Binanace extends Market {
     this.market  = new ccxt[exchangeId];
   }
 
-  bn (number) {
-    return new this.w3.utils.BN(number);
-  }
-
   async getRate(currency) {
     const pair = await this.market.fetchTicker('RCN/' + currency);
     return pair.info.lastPrice;
