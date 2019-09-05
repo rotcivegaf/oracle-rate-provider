17a46900633dd68e791fb276711d934904382983
Nicolas <menendeznm@gmail.com>
2019-09-05 11:18:20 -0300

    Add command options to decrypt keystore file and get private key

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const program = require('commander');
 const Marmo = require('marmojs');
 const Provider = require('./src/Provider.js');
 const { w3, instanceSigners, instanceOracleFactory, instanceOracles } = require('./src/constructors.js');
-const { sleep } = require('./src/utils.js');
+const { sleep, importFromFile } = require('./src/utils.js');
+
+
+async function pkFromKeyStore(address, key) {
+  var keyObject = importFromFile(address);
+
+  const decrypted = w3.eth.accounts.decrypt(keyObject, key);
+
+  return decrypted.privateKey;
+}
 
 async function main() {
   program
@@ -19,11 +28,23 @@ async function main() {
       '-w, --wait <wait>',
       'The time to wait for a new provide',
       15
+    )
+    .option(
+      '-k, --key <key>',
+      'key passphrase to decrypt keystoreFile',
+      ''
+    )
+    .option(
+      '-a, --address <address>',
+      'address of private key to decrypt keystoreFile',
+      ''
     );
 
   program.parse(process.argv);
 
-  const pk = program.PK ? program.PK : program.filePk ? program.filePk[0] : process.env.PK;
+  const pk = program.PK ? program.PK : program.filePk ?
+    program.filePk[0] : process.env.PK ? process.env.PK : await pkFromKeyStore(program.address, program.key);
+
   const wait = program.wait ? program.wait : process.env.WAIT;
   const waitMs = wait * 60 * 1000;
 
