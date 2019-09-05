3f350c7462572a18fb4570c1189c8f2c0798c01d
Nicolas <menendeznm@gmail.com>
2019-09-05 11:16:19 -0300

    Add function to import keyObject from keystore file

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,56 @@
 module.exports.sleep = async (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
+
+
+/**
+  * Import key data object from keystore JSON file.
+  * @param {string} address Ethereum address to import.
+  * @param {string=} datadir Ethereum data directory (default: ~/.ethereum).
+  * @param {function=} cb Callback function (optional).
+  * @return {Object} Keystore data file's contents.
+  */
+module.exports.importFromFile = (address, datadir, cb) => {
+  var keystore, filepath, path, fs;
+  path = require('path');
+  fs = require('fs');
+  address = address.replace('0x', '');
+  address = address.toLowerCase();
+
+  function findKeyfile(keystore, address, files) {
+    var i, len, filepath = null;
+    for (i = 0, len = files.length; i < len; ++i) {
+      if (files[i].indexOf(address) > -1) {
+        filepath = path.join(keystore, files[i]);
+        if (fs.lstatSync(filepath).isDirectory()) {
+          filepath = path.join(filepath, files[i]);
+        }
+        break;
+      }
+    }
+    return filepath;
+  }
+
+  function isFunction(f) {
+    return typeof f === 'function';
+  }
+
+  datadir = datadir || path.join(process.env.HOME, '.ethereum');
+  keystore = path.join(datadir, 'keystore');
+  if (!isFunction(cb)) {
+    filepath = findKeyfile(keystore, address, fs.readdirSync(keystore));
+    if (!filepath) {
+      throw new Error('could not find key file for address ' + address);
+    }
+    return JSON.parse(fs.readFileSync(filepath));
+  }
+  fs.readdir(keystore, function (ex, files) {
+    var filepath;
+    if (ex) return cb(ex);
+    filepath = findKeyfile(keystore, address, files);
+    if (!filepath) {
+      return new Error('could not find key file for address ' + address);
+    }
+    return cb(JSON.parse(fs.readFileSync(filepath)));
+  });
+};
